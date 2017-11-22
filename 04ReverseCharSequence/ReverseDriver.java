b69d9fc9c95c50a8fe99b9c92be5b1e67627f0f5
jchen59 <jchen59@stuy.edu>
2017-11-22 14:42:25 -0500

    adding ReverseCharSequence

---
+++ 04ReverseCharSequence/ReverseDriver.java
@@ -0,0 +1,10 @@
+public class ReverseDriver {
+    public static void main(String[] args) {
+	ReverseCharSequence a = new ReverseCharSequence("emsisihtolleh");
+	System.out.println(a.charAt(3));
+	System.out.println(a.length());
+	System.out.println(a.subSequence(1,5));
+	System.out.println(a);
+    }
+
+}
