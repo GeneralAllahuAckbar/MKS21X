d7ff8f68d2687ad7f7699bc377780edd9e44b3af
jchen59 <jchen59@stuy.edu>
2017-11-28 14:42:46 -0500

    added getters setters and compareTo

--- 05Library/LibraryBook.java
+++ 05Library/LibraryBook.java
@@ -1,10 +1,25 @@
 public abstract class LibraryBook extends Book implements Comparable<LibraryBook> {
+    private String callNumber;
     public LibraryBook(String author, String title, String isbn, String callNumber) {
 	super(author, title, isbn);
 	this.callNumber = callNumber;
+    }
+    public abstract void checkout(String patron, String due);
+    public abstract void returned();
+    public abstract String circulationStatus();
 
+    public int compareTo(LibraryBook other) {
+	return callNumber.compareTo(other.callNumber);
     }
 
+    public String getCallNumber() {
+	return callNumber;
+    }
+    public void setCallNumber(String call){
+	callNumber = call;
+    }
+    
 
+    
 
 }
