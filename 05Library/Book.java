d437a06f9ec4b29777a35aaadb515cc8ffbaf0ab
jchen59 <jchen59@stuy.edu>
2017-11-28 14:19:29 -0500

    added constructor

---
+++ 05Library/Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    private String title,author,isbn;
+
+    public Book() {
+    }
+
+    public Book(String author, String title, String isbn) {
+	this.author = author;
+	this.title = title;
+	this.isbn = isbn;
+    }
+
+}
