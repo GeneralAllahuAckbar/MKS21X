de19874df526592c74e1dccc9a381746f99e0778
jchen59 <jchen59@stuy.edu>
2017-12-07 14:47:28 -0500

    added insertiion sort

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -4,32 +4,40 @@ public class Sorts{
     return "09.Chen.Jiayang"; 
   }
 
-  /**Selection sort of an int array. 
-  *Upon completion, the elements of the array will be in increasing order.
-  *@param data  the elements to be sorted.
-  */
+    public static void swap(int[] array, int a, int b) {
+	int supercool = array[a];
+	array[a] = array[b];
+	array[b] = supercool;
+    }
   public static void selectionSort(int[] data){
       for (int i = 0; i < data.length; i++) {
-	  int min = 0;
-	  int a;
-	  for (a=i; a < data.length; a++) {
+	  int min = i;
+	  for (int a=i; a < data.length; a++) {
 	      if (data[min] > data[a]) {
 		  min = a;
 	      }
 	  }
-	  int holder = data[min];
-	  data[min] = data[i];
-	  data[i] = holder;
+	  swap(data, min, i);
       }
-
-
   }
 
+    public static void insertionSort(int[] data) {
+	for (int i = 1; i < data.length; i++) {
+	    int j = i;
+	    while (j > 0 && data[j] < data[j-1]) {
+	        swap(data, j ,j-1);
+		j--;
+	    }
+	}
+    }
 
     public static void main(String[] args) {
 	int[] a = {3,41,21,2,1,6};
 	selectionSort(a);
 	System.out.println(Arrays.toString(a));
+	int[] b = {3,41,21,2,1,6};
+	insertionSort(b);
+	System.out.println(Arrays.toString(b));
 
     }
 
