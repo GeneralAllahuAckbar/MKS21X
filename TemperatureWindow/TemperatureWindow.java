efa342c5aa332cd08900cf08fb72c603f5372a39
jchen59 <jchen59@stuy.edu>
2017-12-14 14:47:25 -0500

    working added trycatch for numberformatexception

--- TemperatureWindow/TemperatureWindow.java
+++ TemperatureWindow/TemperatureWindow.java
@@ -1,3 +1,4 @@
+import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 
@@ -5,12 +6,12 @@ import java.awt.*;
 public class TemperatureWindow extends JFrame implements ActionListener{
     private JButton toCel,toFar;
     private Container window;
-    private JTextfield number;
+    private JTextField number;
     public static double FtoC (double farenheit) {
 	return (farenheit - 32) * 5 / 9;
     }
     public static double CtoF (double celcius) {
-	return celcius * 9 / 5 + 32;
+	return celcius * 1.8 + 32;
     }
     public TemperatureWindow() {
 	this.setTitle("Temperature Converter");
@@ -18,29 +19,42 @@ public class TemperatureWindow extends JFrame implements ActionListener{
 	this.setLocation(100,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	window = this.getContentPane();
-	pane.setLayout(new FlowLayout());
+	window.setLayout(new FlowLayout());
 	toCel = new JButton("Convert To Celcius");
 	toFar = new JButton("Convert To Farenheit");
-	number = new JTextField("");
+	number = new JTextField(32);
 	window.add(toFar);
-	window.add(toCel);
 	window.add(number);
+	window.add(toCel);	
 	toFar.setActionCommand("tofarenheit");
 	toCel.setActionCommand("tocelcius");
+	toFar.addActionListener(this);
+	toCel.addActionListener(this);
     }
 
     public void actionPerformed(ActionEvent e) {
+	System.out.println(e.getActionCommand());
 	String s = e.getActionCommand();
-	switch (s) {
-	case s.equals("tofarenheit"):
-	    t.setText(CtoF(Integer.parseInt(t.getText()));
-
+	try {
+	    switch (s) {
+	    case "tocelcius":
+	       	number.setText("" +(float) CtoF(Double.parseDouble(number.getText())));
+		break;
+	    case "tofarenheit":
+		number.setText("" +(float) FtoC(Double.parseDouble(number.getText())));
+		break;
+	    }
+	} catch(NumberFormatException event) {
+	    number.setText("Please enter a value");
 	}
-	
+    }
+
+    public static void main(String[] args) {
+	TemperatureWindow g=  new TemperatureWindow();
+	g.setVisible(true);
 
 
     }
-    
 }
 
 
